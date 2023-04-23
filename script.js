a1338e44efe62b60f115f612b8b64576f81af6c4
Pelumi85689 <oyeseguna@gmail.com>
2023-04-23 22:33:58 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let userEmail = document.getElementById("user-email");
+let userPassword = document.getElementById("user-password");
+
+let registeredUsers = JSON.parse(localStorage.getItem("allUsers"));
+
+function signIn(ev) {
+    ev.preventDefault();
+    
+    let authorizedUser = registeredUsers.find((userDetails) => userDetails.email === userEmail.value && userDetails.password === userPassword.value);
+    console.log(authorizedUser);
+
+    if (!authorizedUser) {
+        alert("User details not found")
+    } else {
+        window.location.href = "dashboard.html";
+        localStorage.setItem("currentUser", JSON.stringify(authorizedUser));
+    }
+}
+
+function gotoSignUp() {
+    window.location.href = "signup.html"}
